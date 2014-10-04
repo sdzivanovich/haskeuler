0629a56ab7611d08b784cbcc172078a31818e410
Sheridan Zivanovich <sdzivanovich@gmail.com>
2014-10-04 18:37:17 -0400

    another couple of problems

---
+++ problem7.hs
@@ -0,0 +1,21 @@
+-- project euler: problem 7
+-- https://projecteuler.net/problem=7
+-- What is the 10 001st prime number?
+
+divisibleBy :: (Integral a) => a -> a -> Bool
+x `divisibleBy` y = x `mod` y == 0
+
+isPrime :: (Integral a) => a -> Bool
+isPrime 1       = False
+isPrime 2       = True
+isPrime n  
+    | n <= 0    = error "only natural numbers" 
+    | otherwise =   let sqrtIntegral = ceiling . sqrt . fromIntegral 
+                    in not . any (n `divisibleBy`) $ [2..sqrtIntegral n]
+
+nthPrime :: (Integral a) => Int -> a
+nthPrime n =    let primes = filter isPrime [1..]
+                in primes !! n
+
+-- index by 10000 because lists are 0-indexed
+main = print $ nthPrime 10000
